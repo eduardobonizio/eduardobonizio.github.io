82655ae85a46efa8a869df1a757f57e5ce04a189
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-02-10 00:36:45 -0300

    Voltando ao BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './app/App';
 import store from './store';
