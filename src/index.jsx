8ddbb79f6077933a71b9ae3b1257aac692c860e2
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-02-10 00:40:45 -0300

    Retornando o basename

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <App />
       </Router>
     </Provider>
