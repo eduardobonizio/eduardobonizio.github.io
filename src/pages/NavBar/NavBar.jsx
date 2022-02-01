f213ae84627463df1df7a90c1f17307f7bafe1e4
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-01 15:36:40 -0300

    Tentando despoluir a navbar

--- src/pages/NavBar/NavBar.jsx
+++ src/pages/NavBar/NavBar.jsx
@@ -1,20 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink as RRNavLink } from 'react-router-dom';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-} from 'reactstrap';
+import { NavItem, NavLink, NavbarText } from 'reactstrap';
 
 import app from '../../api/Firebase';
 
@@ -24,74 +11,47 @@ function NavBar() {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <Navbar color="dark" dark expand="sm" className="mb-2">
-      <div className="container">
-        <NavbarBrand tag={RRNavLink} to="/">
-          Home
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink
-                onClick={() => isOpen && toggle()}
-                tag={RRNavLink}
-                to="/game"
-              >
-                Quizz
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                onClick={() => isOpen && toggle()}
-                tag={RRNavLink}
-                to="/30-dias-css"
-              >
-                Desafio CSS
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Guias
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem
-                  onClick={() => isOpen && toggle()}
-                  tag={RRNavLink}
-                  to="/github-pages-react"
-                >
-                  Github pages com react
-                </DropdownItem>
-                {/* <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem> */}
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          {globalUser && (
-            <div>
-              <NavbarText>
-                <div>{globalUser.displayName}</div>
-              </NavbarText>
-              <img
-                src={globalUser.photoURL}
-                alt="User"
-                className="ml-3"
-                style={{ height: '38px', borderRadius: '50%' }}
-              />
-              <button
-                type="button"
-                onClick={() => {
-                  app.auth().signOut();
-                }}
-              >
-                Logout
-              </button>
-            </div>
-          )}
-        </Collapse>
+    <div className="container d-flex">
+      <div>Home</div>
+      <div className="container d-flex">
+        <NavLink onClick={() => isOpen && toggle()} tag={RRNavLink} to="/game">
+          Quizz
+        </NavLink>
+        <NavItem>
+          <NavLink
+            onClick={() => isOpen && toggle()}
+            tag={RRNavLink}
+            to="/30-dias-css"
+          >
+            Desafio CSS
+          </NavLink>
+        </NavItem>
+        <NavItem
+          onClick={() => isOpen && toggle()}
+          tag={RRNavLink}
+          to="/github-pages-react"
+        >
+          Github pages com react
+        </NavItem>
+        <NavbarText>
+          <div>{globalUser.displayName}</div>
+        </NavbarText>
+        <img
+          src={globalUser.photoURL}
+          alt="User"
+          className="ml-3"
+          style={{ height: '38px', borderRadius: '50%' }}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            app.auth().signOut();
+          }}
+        >
+          Logout
+        </button>
       </div>
-    </Navbar>
+    </div>
   );
 }
 
