45a4da7534de3b4e57addd96328edc187383864d
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-05 11:33:36 -0300

    Melhora tela de registro

--- src/pages/UserControll/SignUp.jsx
+++ src/pages/UserControll/SignUp.jsx
@@ -1,72 +1,84 @@
-import React, { useCallback } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import app, { signInWithGoogle } from '../../api/Firebase';
 import image from '../../assets/Index';
 
 function SignUp({ history }) {
   const navigate = useNavigate();
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleSignUp = useCallback(
     async event => {
       event.preventDefault();
+      const TENSECONDS = 10000;
       const { email, password } = event.target.elements;
       try {
         await app
           .auth()
           .createUserWithEmailAndPassword(email.value, password.value);
-        navigate('/');
+        navigate('/quiz');
       } catch (error) {
-        console.log(error);
+        if (!showAlert) {
+          setShowAlert(true);
+          setTimeout(() => {
+            setShowAlert(false);
+          }, TENSECONDS);
+        }
       }
     },
     [history],
   );
 
+  const goToLoginPage = () => navigate('/login');
+
   return (
-    <div className="container d-flex flex-column align-items-center">
+    <div className="container d-flex flex-column align-items-center input-groups">
       <h1>Sign up</h1>
-      <div className="d-flex flex-column align-items-center">
-        <form onSubmit={handleSignUp} className="d-flex flex-column">
-          <label htmlFor="email" className="d-flex flex-column">
-            Email
-            <input name="email" type="email" placeholder="Email" />
-          </label>
-          <label htmlFor="password" className="d-flex flex-column">
-            Password
-            <input name="password" type="password" placeholder="Password" />
-          </label>
-          <button
-            type="submit"
-            style={{
-              width: '100%',
-              backgroundColor: 'transparent',
-              border: 'none',
-            }}
-          >
-            Sign Up
-          </button>
-        </form>
+      <form onSubmit={handleSignUp} className="d-flex flex-column">
+        <input
+          type="email"
+          className="form-control mb-1"
+          placeholder="Email"
+          aria-label="Email"
+          aria-describedby="basic-addon1"
+          name="email"
+        />
+        <input
+          type="password"
+          className="form-control mb-1"
+          placeholder="Password"
+          aria-label="Password"
+          aria-describedby="basic-addon1"
+          name="password"
+        />
+        <button type="submit" className="btn btn-primary mb-1">
+          Register
+        </button>
         <button
           type="submit"
-          style={{
-            width: '100%',
-            backgroundColor: 'transparent',
-            border: 'none',
-          }}
+          className="btn btn-primary mb-1"
+          onClick={goToLoginPage}
         >
-          <Link to="/login">Login</Link>
+          Login
         </button>
-        <button
-          type="button"
-          style={{
-            backgroundColor: 'transparent',
-            border: 'none',
-          }}
-          onClick={() => signInWithGoogle()}
-        >
-          <img src={image.GoogleLoginButton} alt="Login with google" />
-        </button>
-      </div>
+      </form>
+      <button
+        type="button"
+        className="mb-1"
+        style={{
+          backgroundColor: 'transparent',
+          border: 'none',
+        }}
+        onClick={() => signInWithGoogle()}
+      >
+        <img src={image.GoogleLoginButton} alt="Login with google" />
+      </button>
+      {showAlert && (
+        <div className="alert alert-danger" role="alert">
+          Usuário já cadastrado
+        </div>
+      )}
     </div>
   );
 }
