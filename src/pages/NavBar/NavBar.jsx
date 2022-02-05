ceb0443e41b97a3fb538fd1d022153a01be842b9
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-04 21:37:09 -0300

    Adiciona hamburguer button e primeiras estilizações

--- src/pages/NavBar/NavBar.jsx
+++ src/pages/NavBar/NavBar.jsx
@@ -11,33 +11,60 @@ function NavBar() {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className="container d-flex">
-      <div>Home</div>
-      <div className="container d-flex">
-        <Link to="/game">Quizz</Link>
-        <Link to="/30-dias-css">Desafio CSS</Link>
-        <Link to="/github-pages-react">Github pages com react</Link>
-        {globalUser && (
-          <div>
-            <span>
-              <div>{globalUser.displayName}</div>
-            </span>
-            <img
-              src={globalUser.photoURL}
-              alt="User"
-              className="ml-3"
-              style={{ height: '38px', borderRadius: '50%' }}
-            />
-            <button
-              type="button"
-              onClick={() => {
-                app.auth().signOut();
-              }}
-            >
-              Logout
-            </button>
-          </div>
-        )}
+    <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar-itens"
+          aria-controls="navbar-itens"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbar-itens">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <a href="/game" className="nav-item">
+              Quizz
+            </a>
+            <a href="/30-dias-css" className="nav-item">
+              Desafio CSS
+            </a>
+            <a href="/github-pages-react" className="nav-item">
+              Github pages com react
+            </a>
+            {globalUser && (
+              <>
+                <a href="/">
+                  <div>{globalUser.displayName}</div>
+                </a>
+                <img
+                  src={globalUser.photoURL}
+                  alt="User"
+                  className="ml-3"
+                  style={{
+                    maxHeight: '40px',
+                    maxWidth: '40px',
+                    borderRadius: '50%',
+                  }}
+                />
+                <button
+                  type="button"
+                  onClick={() => {
+                    app.auth().signOut();
+                  }}
+                >
+                  Logout
+                </button>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
