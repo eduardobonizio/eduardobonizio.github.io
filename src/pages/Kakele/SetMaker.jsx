f5d360106e1f426e85acaa8ca02e725d4a1210e5
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-06 13:05:11 -0300

    Limpa o arquivo para começar o set maker

--- src/pages/Kakele/SetMaker.jsx
+++ src/pages/Kakele/SetMaker.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
@@ -5,6 +6,8 @@ export default function SetMaker() {
   return (
     <div className="container d-flex justify-content-center flex-column">
       Set Maker
+      <div className="container" id="wiki-data-container" />
+      {}
     </div>
   );
 }
