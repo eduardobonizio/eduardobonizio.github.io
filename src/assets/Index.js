e387dfd44718f01e857efc87ee97168f0cadb8b3
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-07 12:17:19 -0300

    Remove imagens que não estavam sendo utilizadas

--- src/assets/Index.js
+++ src/assets/Index.js
@@ -1,27 +1,5 @@
 import GoogleLoginButton from './google_signin_buttons/web/1x/btn_google_signin_dark_pressed_web.png';
-import FolderStructure from './Images/FolderStructure.png';
-import GhPagesBranch from './Images/GhPagesBranch.png';
-import GitHubNewRepo from './Images/GitHubNewRepo.png';
-import HomePagePackageJson from './Images/HomePagePackageJson.png';
-import Html404 from './Images/Html404.png';
-import IndexHtml from './Images/IndexHtml.png';
-import IndexJs from './Images/IndexJs.png';
-import PackageJson from './Images/PackageJson.png';
-import Redirecting from './Images/Redirecting.gif';
-import RepoSSH from './Images/RepoSSH.png';
-import SettingsDoRepositorio from './Images/SettingsDoRepositorio.gif';
 
 export default {
-  FolderStructure,
-  GhPagesBranch,
-  GitHubNewRepo,
-  HomePagePackageJson,
-  Html404,
-  IndexHtml,
-  IndexJs,
-  PackageJson,
-  Redirecting,
-  RepoSSH,
-  SettingsDoRepositorio,
   GoogleLoginButton,
 };
