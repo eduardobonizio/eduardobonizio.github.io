a2fd1d68382babf3618f5623774e8691fbe82f6c
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-07 15:12:42 -0300

    Passa funcao para o arquivo correto e importa

--- src/pages/Kakele/ShowSet.jsx
+++ src/pages/Kakele/ShowSet.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import { urlParamsToObject } from './kakele';
+
 export default function ShowSet() {
   const urlParams = useParams();
-  const itens = `{"${urlParams['*']
-    .replaceAll('=', '":"')
-    .replaceAll(' ', '","')
-    .replaceAll('-', ' ')}"}`;
-  const itensObject = JSON.parse(itens);
-  console.log(itensObject);
-
+  const itens = urlParamsToObject(urlParams);
+  console.log(itens);
   return (
     <div className="container d-flex justify-content-center flex-column">
       Mostrar Set
