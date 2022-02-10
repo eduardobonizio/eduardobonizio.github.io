cb5ca9b5af2a21182a1d07da5106a87dfdf95e50
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-10 14:34:59 -0300

    Adiciona estilo para os botoes

--- src/pages/Kakele/Componentes/ButtonForKakele.jsx
+++ src/pages/Kakele/Componentes/ButtonForKakele.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './ButtonForKakele.css';
+
 export default function ButtonForKakele(props) {
   const { onClick, text } = props;
   return (
     <button
       type="button"
-      className="btn btn-light mb-2"
+      className="btn btn-light mb-2 button-for-kakele"
       onClick={() => onClick()}
     >
       {text}
