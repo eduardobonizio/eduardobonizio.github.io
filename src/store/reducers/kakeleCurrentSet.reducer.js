dc4e3bb53c08d9c588fa7d528416bfbd230ff1f5
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-12 18:42:21 -0300

    Corrige o reducer

--- src/store/reducers/kakeleCurrentSet.reducer.js
+++ src/store/reducers/kakeleCurrentSet.reducer.js
@@ -1,10 +1,22 @@
-const CURRENT_SET = {};
+const CURRENT_SET = {
+  necklace: {},
+  helmet: {},
+  ring: {},
+  weapon: {},
+  armor: {},
+  shield: {},
+  book: {},
+  accessorie: {},
+  leg: {},
+  shoe: {},
+};
 
 export default function reducer(state = CURRENT_SET, action) {
   const { type, payload } = action;
+
   switch (type) {
     case 'UPDATE_CURRENT_SET':
-      return { ...CURRENT_SET, [payload.slot]: payload };
+      return { ...state, [payload.slot]: payload };
 
     default:
       return state;
