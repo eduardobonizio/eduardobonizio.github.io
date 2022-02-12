85433aa73cb0c40d785cef4cceab9e0481809bbc
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-12 13:03:09 -0300

    Muda nome do arquivo da action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,5 @@
 import * as currentLoggedUser from './currentLoggedUser.actions';
-import * as kakeleActions from './kakele.actions';
+import * as kakeleActions from './kakeleCurrentSet.actions';
 
 const actions = {
   currentLoggedUser,
