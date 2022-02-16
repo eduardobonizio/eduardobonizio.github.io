12bd3b5ca032ec41b80ae0806007ead8f79f6a69
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-16 20:34:14 -0300

    Corrige rotas

--- src/pages/Kakele/Kakele.jsx
+++ src/pages/Kakele/Kakele.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useOutletContext } from 'react-router-dom';
 
 export default function Kakele() {
+  const language = useOutletContext();
+  console.log(language);
   return (
     <div className="container d-flex justify-content-center flex-column">
       <div className="container d-flex justify-content-around mb-2">
-        <Link to="/kakele/set">Ver Set</Link>
-        <Link to="/kakele/set-maker">Gerar set</Link>
-        <Link to="/kakele/search-item">Procurar Item</Link>
-        <Link to="/kakele/ore-calculator">Minérios</Link>
+        <Link to="set">Ver Set</Link>
+        <Link to="set-maker">Gerar set</Link>
+        <Link to="search-item">Procurar Item</Link>
+        <Link to="ore-calculator">Minérios</Link>
       </div>
       <Outlet />
     </div>
