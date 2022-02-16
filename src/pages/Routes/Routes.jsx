74bbb90a2773c35de5c67625e61ec66b1aec0a8c
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-16 16:33:40 -0300

    Remove ga porque nao consegui fazer funcionar

--- src/pages/Routes/Routes.jsx
+++ src/pages/Routes/Routes.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import PrivateRoute from '../../api/PrivateRoute';
 import Home from '../Home/Home';
@@ -16,24 +15,7 @@ import StartGame from '../Quiz/StartGame';
 import Login from '../UserControll/Login';
 import SignUp from '../UserControll/SignUp';
 
-function usePageViews() {
-  // eslint-disable-next-line immutable/no-let
-  const location = useLocation();
-
-  useEffect(() => {
-    if (!window.GA_INITIALIZED) {
-      ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_CODE);
-      // eslint-disable-next-line immutable/no-mutation
-      window.GA_INITIALIZED = true;
-    }
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  }, [location]);
-}
-
 export default function Rotas() {
-  usePageViews();
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
